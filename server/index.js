ede9d27194f7ca364b282e35e889732a0c928f1d
Muhammad Danish <danishpk175@gmail.com>
2023-07-19 14:57:40 +0530

    UPDATED THE MONGOOSE

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import Connection from './database/db.js';
 
 const app = express();
 
 const PORT = 8000;
+
+Connection();
 
 app.listen(PORT, () => console.log(`The server is running successfully on port ${PORT}`));
