a84ebfffcbb1f46ee52e422d3146c4264fd353ad
Muhammad Danish <danishpk175@gmail.com>
2023-07-19 22:44:03 +0530

    Update in Redux

--- client/src/components/asap.js
+++ client/src/components/asap.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import {useState, React} from 'react';
+import './asap.css';
 
 const Asap = () => {
+  const [text, setText] = useState("");
+
+  const onFormSubmit = () => {
+
+  }
+
+  const onInputChange = () => {
+    
+  }
+ 
   return (
     <>
-    <h1>Hello World</h1>
-    <p>This to test the working of the page Asap</p>
+    <div className='asap-css'>
+      <h1>Hello World</h1>
+      <p>This is to test the working of the page Asap</p>
+      
+      <form className='form' onSubmit={onFormSubmit}>
+        <input 
+          placeholder='Enter something'
+          className='input'
+          onChange={onInputChange}
+        />
+      </form>
+    </div>
     </>
   );
 }
