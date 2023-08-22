a866755bd4a0224524e3d49be52ac6ef1a91f4c8
Muhammad Danish <danishpk175@gmail.com>
2023-08-22 12:01:33 +0530

    Created controllers, models, and update in database

--- server/index.js
+++ server/index.js
@@ -1,35 +1,80 @@
 import express from 'express';
 import cors from 'cors';
+import { register } from "./controller/auth.js"
 import Routes from './routes/route.js';
 import bodyParser from 'body-parser';
 import Connection from './database/db.js';
 import multer from 'multer';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import helmet from 'helmet';
 import morgan  from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 
-// Configurations
-
-
-
-
-
-
-
+/* CONFIGURATION */
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+dotenv.config();
 const app = express();
-
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin" }));
+app.use(morgan("common")); 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(bodyParser.json({extended: true}));
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
-app.use('/', Routes);
 
-const PORT = 8000;
 
-Connection();
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
-app.listen(PORT, () => console.log(`The server is running successfully on port ${PORT}`));
+const upload = multer({ storage });
+  
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register)
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then( () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch( (error) => console.log(`${error} did not connect`));
+
+
+
+
+
+
+
+
+
+/*
+    
+    const app = express();
+
+    app.use(cors());
+    app.use(bodyParser.json({extended: true}));
+    app.use(bodyParser.urlencoded({extended: true})); 
+
+    app.use('/', Routes);
+
+    const PORT = 8000;
+
+    Connection();
+
+    app.listen(PORT, () => console.log(`The server is running successfully on port ${PORT}`)); 
+
+*/
