fd37423ea7f81b4d0630d33884fb1ad49c147da1
Muhammad Danish <danishpk175@gmail.com>
2023-08-23 18:29:14 +0530

    Routes for add and remove Friends.
    By using Promise.all and map to iterate for get the User Friends and add/remove Friends.

--- server/controller/users.js
+++ server/controller/users.js
@@ -40,10 +40,26 @@ export const addRemoveFriend = async (req, res) => {
         if(user.friends.includes(friendId)) {
             user.friends = user.friends.filter((id) => id !== friendId);
             friend.friends = friend.friends.filter((id) => id !== id);
-            
+        }else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
         }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath }
+            }
+        );
+
+        res.status(200).json(formattedFriends);
 
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
-}
+};
