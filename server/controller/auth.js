24cbc282062556963b71e6920268df6c664873d8
Muhammad Danish <danishpk175@gmail.com>
2023-08-24 15:23:48 +0530

    Update.

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,12 +1,12 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from './models/User.js';
+import User from '../models/User.js';
 
 /* REGISTER USER */
 export const register = async (req, res) => {
     try {
         const {
-            firstName, 
+            firstName,
             lastName,
             email,
             password,
@@ -20,7 +20,7 @@ export const register = async (req, res) => {
         const passwordHash = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            firstName, 
+            firstName,
             lastName,
             email,
             password: passwordHash,
@@ -43,12 +43,12 @@ export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email: email });
-        if (!user) return res.status(400).json({ msg: "User does not exist. "});
+        if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch) return res.status(400).json({ msg: "Invaild credentials. "});
+        if (!isMatch) return res.status(400).json({ msg: "Invaild credentials. " });
 
-        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         delete user.password;
         res.status(200).json({ token, user });
 
---
+++ server/routes/post.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { getFeedPosts, getUserPosts, likePost } from "../controller/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts, getFeedPosts);
+router.get("/:userId/post")
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
+ 
