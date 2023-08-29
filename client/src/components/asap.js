e531d9ea863c6125fe96c0038c5b5527ec19a828
Muhammad Danish <danishpk175@gmail.com>
2023-08-29 12:45:11 +0530

    Update in Route.

--- client/src/components/asap.js
+++ client/src/components/asap.js
@@ -1,45 +1,43 @@
-import { useState, React } from 'react';
+// import { useState, React } from 'react';
 
-import './css/asap.css';
+// import './css/asap.css';
 
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
-import { addNewTodo } from '../redux/actions';
+// const Asap = () => {
+//   const [text, setText] = useState("");
 
-const Asap = () => {
-  const [text, setText] = useState("");
+//   const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+//   const onFormSubmit = (e) => {
+//     e.preventDefault();
 
-  const onFormSubmit = (e) => {
-    e.preventDefault();
+//     dispatch(addNewTodo(text));
 
-    dispatch(addNewTodo(text));
+//     setText('');
+//   }
 
-    setText('');
-  }
+//   const onInputChange = (e) => {
+//     setText(e.target.value);
+//   }
 
-  const onInputChange = (e) => {
-    setText(e.target.value);
-  }
+//   return (
+//     <>
+//       <div className='asap-css'>
+//         <h1 className='asapH'>Hello World</h1>
+//         <p className='asapP'>This is to test the working of the page Asap</p>
 
-  return (
-    <>
-      <div className='asap-css'>
-        <h1 className='asapH'>Hello World</h1>
-        <p className='asapP'>This is to test the working of the page Asap</p>
+//         <form className='form' onSubmit={onFormSubmit}>
+//           <input
+//             placeholder='Enter something'
+//             className='asapInput'
+//             onChange={onInputChange}
+//             value={text}
+//           />
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
 
-        <form className='form' onSubmit={onFormSubmit}>
-          <input
-            placeholder='Enter something'
-            className='asapInput'
-            onChange={onInputChange}
-            value={text}
-          />
-        </form>
-      </div>
-    </>
-  );
-}
-
-export default Asap;
+// export default Asap;
